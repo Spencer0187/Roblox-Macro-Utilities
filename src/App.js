751be7e9c38de57a07867599223c1d2bdd8a1bdc
Spencer0187 <125307856+Spencer0187@users.noreply.github.com>
2025-02-21 21:07:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import styled, { createGlobalStyle, keyframes } from 'styled-components';
 
 // ===== Animation Definitions =====
